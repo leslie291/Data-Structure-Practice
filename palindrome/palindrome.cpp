a6c36b2ae1228cac41a5606181f4e3fe21a99c48
leslie291 <lesles078@gmail.com>
2026-03-12 03:10:36 -0700

    Add palindrome: two pointers O(n) time O(1) space

---
+++ palindrome/palindrome.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(std:: string s){
+    //grap the index
+    int left = 0; 
+    int right = s.size() -1; 
+    while(left < right){
+        if(s[left] != s[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true; 
+}
+
+int main(){
+    cout << isPalindrome("racecar") << endl;
+    cout << isPalindrome("hello") << endl;
+    return 0;
+}
